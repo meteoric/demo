c87185bdbfc5208a5ec5fe98b1af992757c8729d
Nick Wientge <nick@exygeninteractive.com>
2014-12-27 17:24:20 -0500

    add manual platform override and some custom platform icons

--- both/router.js
+++ both/router.js
@@ -2,6 +2,15 @@ Router.configure({
   layoutTemplate: 'layout'
 });
 
+Meteor.startup(function () {
+  if (Meteor.isClient) {
+    var location = Iron.Location.get();
+    if (location.queryObject.platformOverride) {
+      Session.set('platformOverride', location.queryObject.platformOverride);
+    }
+  }
+});
+
 Router.map(function() {
   this.route('index', {path: '/'});
   this.route('actionSheet');
